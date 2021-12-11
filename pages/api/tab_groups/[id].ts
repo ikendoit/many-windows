1ad5364be9460752c0859848c35f448e3a3ab754
Ken Nguyen <ikendoit1998@gmail.com>
2021-12-10 22:30:24 -0800

    Fixed bug unknown req object in api

--- pages/api/tab_groups/[id].ts
+++ pages/api/tab_groups/[id].ts
@@ -1,6 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { useRouter } from 'next/router'
+type Data = {
+  name: string
+}
 
-const Post = () => {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   if (req.method !== 'POST') {
     res.status(405).send({ message: 'Only POST requests allowed' })
     return
@@ -8,7 +15,5 @@ const Post = () => {
 
   const body = JSON.parse(req.body)
 
-  return {'message': 'That post was smoooth'}
+  res.status(200).json({'message': 'That post was smoooth'})
 }
-
-export default Post
